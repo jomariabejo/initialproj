7bcddace196c29ed86ded7d4732bcacc3f55e3de
jomariabejo <70135786+jomariabejo@users.noreply.github.com>
2024-12-21 11:02:47 +0800

    chore: add unit tests for 'retrieve all tasks' and 'retrieve tasks by priority' routes

--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -1,22 +1,47 @@
+package com.example
+
 import com.jomariabejo.ktor.module
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
-import junit.framework.TestCase.assertEquals
-import kotlin.test.Test
-import kotlin.test.assertNotEquals
+import org.junit.Test
+import kotlin.test.assertContains
+import kotlin.test.assertEquals
+
 
 class ApplicationTest {
     @Test
-    fun testRoot(): Unit = testApplication {
+    fun tasksCanBeFoundByPriority() = testApplication {
         application {
             module()
         }
 
-        // Test Helloworld
-        val response = client.get("/")
+        val response = client.get("/tasks/byPriority/Medium")
+        val body = response.bodyAsText()
+
         assertEquals(HttpStatusCode.OK, response.status)
-        assertEquals("Hello World!", response.bodyAsText())
+        assertContains(body, "Mow the lawn")
+        assertContains(body, "Paint the fence")
+    }
+
+    @Test
+    fun invalidPriorityProduces400() = testApplication {
+        application {
+            module()
+        }
+
+        val response = client.get("/tasks/byPriority/Invalid")
+        assertEquals(HttpStatusCode.BadRequest, response.status)
+    }
+
+    @Test
+    fun unusedPriorityProduces404() = testApplication {
+        application {
+            module()
+        }
+
+        val response = client.get("/tasks/byPriority/Vital")
+        assertEquals(HttpStatusCode.NotFound, response.status)
     }
 }
