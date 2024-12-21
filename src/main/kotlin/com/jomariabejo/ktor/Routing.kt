28dbecd6bc73d25ebcdc92d043f7a19ad08a230a
jomariabejo <70135786+jomariabejo@users.noreply.github.com>
2024-12-21 10:57:52 +0800

    chore: render tasks as an HTML table for better visibility

--- src/main/kotlin/com/jomariabejo/ktor/Routing.kt
+++ src/main/kotlin/com/jomariabejo/ktor/Routing.kt
@@ -5,6 +5,8 @@ import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import model.TaskRepository
+import tasksAsTable
 
 fun Application.configureRouting() {
     routing {
@@ -17,6 +19,13 @@ fun Application.configureRouting() {
             val text = "<h1>Hello From Ktor</h1>"
             val type = ContentType.parse("text/html")
             call.respondText(text, type)
+        }
+        get("/tasks") {
+            val tasks = TaskRepository.allTasks()
+            call.respondText(
+                contentType = ContentType.parse("text/html"),
+                text = tasks.tasksAsTable()
+            )
         }
     }
 }
